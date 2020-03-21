69b72a4a7a4e4737d2ff7ff2e77d63f6d8e8a0fd
dafinrs <tuupai@dafinoer.local>
2020-03-21 23:29:27 +0700

    fisrt commit

---
+++ calculator_boss/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator_boss
+//
+//  Created by dafinrs on 21/03/20.
+//  Copyright © 2020 dafinrs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
